af86d112c1d109417ba49a63262640318a38f3b8
maxiweed <max.canito@hotmail.com>
2020-11-06 01:15:36 -0300

    validacion credenciales login :)

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,6 @@
-import {React,Fragment} from 'react';
+import {React, Fragment, useState} from 'react';
 import { Link } from 'react-router-dom';
+import '../../css/Login.css'
 
 import { Button,Form,Col,Row,Container, Card} from 'react-bootstrap';
 
@@ -7,31 +8,94 @@ import { Button,Form,Col,Row,Container, Card} from 'react-bootstrap';
 
 const Login = (props) => {
 
-     
+     //agregar state de login
+     const [Login, cambiarStateLogin] = useState({
+         username: '',
+         password: ''
+     })
 
+     //actualizar state login
+     const actualizarState = (e) =>{
+        cambiarStateLogin({
+            ...Login,
+            [e.target.name] : e.target.value
+        })
+     }
+
+         //estado para cambiar mensaje de error
+         const [mensajeError, estadoMensaje] = useState('');
+
+        //estado para cambiar color mensaje
+        const [claseError, estadoClaseMensaje] = useState('');
+
+        //obtener valoresd actuales de input distrotion
+        const {username,password} = Login;
+
+        //validar Credenciales 
+        const submitLogin = e =>{
+
+            e.preventDefault();
+            if(username.trim() ==='' ){
+                estadoMensaje('ingrese username');
+                estadoClaseMensaje('error');
+                e.target[0].focus();
+                return;
+            }else{
+                if(password.trim()===''){
+                    estadoMensaje('ingrese password');
+                    estadoClaseMensaje('error');
+                    e.target[1].focus();
+                    return;
+                }else{
+                    estadoMensaje('Enviando....');
+                    estadoClaseMensaje('enviado');
+                }
+            }
+            
+        }
     return ( 
        
         <Fragment>
-            <Col xs={12} sm={8} md={4}>fila uno </Col><br/><br/>
+            <Col xs={12} sm={8} md={4}>
+                
+            </Col><br/><br/>
             <Col xs={12} sm={8} md={4} ><br/><br/>
                  <h2>Login de Ingreeso</h2>
                 <Card style={{ width: '100%', padding:'1em' }}>
-                    <Form>
+                    <Form onSubmit={submitLogin} >
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Ingrese Email</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Control 
+                                    type="text"
+                                    name="username"
+                                    placeholder="Ingrese Username"
+                                    onChange={actualizarState}
+                                    value={username}
+
+                                />
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label> Ingrese Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control 
+                                    type="password"
+                                    name="password"
+                                    placeholder="Ingrese password"
+                                    onChange={actualizarState}
+                                    value={password}
+                            />
                         </Form.Group>
 
-                        <Button variant="primary" type="submit">
-                             Ingresar
+                        <Button
+                            type="submit"
+                            className="btn btn-block ">
+                            Ingresar
                         </Button>
 
                         </Form>
+                        <div>
+                             <h5 className={claseError}>{mensajeError}</h5>
+                         </div>
                     </Card>
                     <Link to={'/RecuperarPassword'}>Recuperar Password</Link><br/>
                     <Link to={'/NuevaCuenta'}>Obtener Cuenta</Link><br/>
